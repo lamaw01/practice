d2dd74f664362e2c0e3961a45ea0a28ba584f85c
lamaw01 <dumaog.janrey@gmail.com>
2021-01-01 18:25:14 +0800

    minor bug

--- lib/screen/scrollview.dart
+++ lib/screen/scrollview.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
-// import 'package:scroll_to_index/scroll_to_index.dart';
+// ignore: unused_import
+import 'package:visibility_detector/visibility_detector.dart';
 
 class ScrollWidget extends StatefulWidget {
   @override
   _ScrollWidgetState createState() => _ScrollWidgetState();
 }
 
-class _ScrollWidgetState extends State<ScrollWidget> {
-  final scrollController = ScrollController();
-  TabController tabController;
+class _ScrollWidgetState extends State<ScrollWidget>
+    with SingleTickerProviderStateMixin {
   // ignore: non_constant_identifier_names
   List<Map<String, dynamic>> list_name = [
     {
@@ -17,15 +17,15 @@ class _ScrollWidgetState extends State<ScrollWidget> {
       "image": "assets/images/jollibee_header.jpg",
       "food1": {
         "name": "Chicken Joy",
-        "price": "90.0",
+        "price": "P 90.00",
       },
       "food2": {
         "name": "Spaghetti",
-        "price": "75.0",
+        "price": "P 75.00",
       },
       "food3": {
         "name": "Burger Steak",
-        "price": "50.0",
+        "price": "P 50.00",
       }
     },
     {
@@ -33,15 +33,15 @@ class _ScrollWidgetState extends State<ScrollWidget> {
       "image": "assets/images/greenwich_header.jpg",
       "food1": {
         "name": "Pineapple Pizza",
-        "price": "150.0",
+        "price": "P 150.00",
       },
       "food2": {
         "name": "Italian Pizza",
-        "price": "75.0",
+        "price": "P 75.00",
       },
       "food3": {
         "name": "Pepper Pizza",
-        "price": "220.0",
+        "price": "P 200.00",
       }
     },
     {
@@ -49,15 +49,15 @@ class _ScrollWidgetState extends State<ScrollWidget> {
       "image": "assets/images/mcdonalds_header.jpg",
       "food1": {
         "name": "Burger",
-        "price": "45.0",
+        "price": "P 45.00",
       },
       "food2": {
         "name": "Fries",
-        "price": "35.0",
+        "price": "P 35.00",
       },
       "food3": {
         "name": "McFloat",
-        "price": "25.0",
+        "price": "P 25.00",
       }
     },
     {
@@ -65,15 +65,15 @@ class _ScrollWidgetState extends State<ScrollWidget> {
       "image": "assets/images/chowking_header.jpg",
       "food1": {
         "name": "Chowfan",
-        "price": "65.0",
+        "price": "P 65.00",
       },
       "food2": {
         "name": "Noodles",
-        "price": "40.0",
+        "price": "P 40.00",
       },
       "food3": {
         "name": "Shiopao",
-        "price": "30.0",
+        "price": "P 30.00",
       }
     },
     {
@@ -81,22 +81,31 @@ class _ScrollWidgetState extends State<ScrollWidget> {
       "image": "assets/images/bigbys_header.jpg",
       "food1": {
         "name": "Belly Buster",
-        "price": "165.0",
+        "price": "P 165.00",
       },
       "food2": {
         "name": "Chicken Corn",
-        "price": "100",
+        "price": "P 100.00",
       },
       "food3": {
         "name": "Longganisa",
-        "price": "65.0",
+        "price": "P 65.00",
       }
     },
   ];
-
+  final scrollController = ScrollController();
+  TabController tabController;
   ItemScrollController _controller;
+  int selectedIndex = 0;
+
   void initState() {
     super.initState();
+
+    tabController = TabController(
+      initialIndex: selectedIndex,
+      length: list_name.length,
+      vsync: this,
+    );
     _controller = ItemScrollController();
   }
 
@@ -104,7 +113,7 @@ class _ScrollWidgetState extends State<ScrollWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: DefaultTabController(
-        initialIndex: 0,
+        initialIndex: selectedIndex,
         length: list_name.length,
         child: NestedScrollView(
           controller: scrollController,
@@ -113,17 +122,18 @@ class _ScrollWidgetState extends State<ScrollWidget> {
               SliverAppBar(
                 pinned: true,
                 bottom: TabBar(
+                  controller: tabController,
                   onTap: (index) => _controller.jumpTo(index: index),
                   isScrollable: true,
                   tabs: List<Widget>.generate(list_name.length, (int index) {
-                    print(index);
+                    // print(index);
                     return Tab(
                       iconMargin: EdgeInsets.symmetric(horizontal: 16.0),
                       // text: list_name[index]['title'] st,
                       child: Text(
                         list_name[index]['title'],
                         style: TextStyle(
-                          fontSize: 22.0,
+                          fontSize: 20.0,
                         ),
                       ),
                     );
@@ -137,67 +147,81 @@ class _ScrollWidgetState extends State<ScrollWidget> {
               itemScrollController: _controller,
               itemCount: list_name.length,
               itemBuilder: (context, index) {
-                return Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(8.0),
+                return VisibilityDetector(
+                  onVisibilityChanged: (VisibilityInfo info) {
+                    if (info.visibleFraction == 1)
+                      setState(() {
+                        selectedIndex = index;
+                        print(selectedIndex);
+                        tabController.animateTo(selectedIndex);
+                      });
+                  },
+                  key: Key(index.toString()),
+                  child: Card(
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            list_name[index]['title'],
-                            style: TextStyle(fontSize: 24.0),
-                            // key: dataKey,
-                          ),
-                          SizedBox(
-                            height: 22.0,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                list_name[index]['food1']['name'],
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                              Text(
-                                list_name[index]['food1']['price'],
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 22.0,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                list_name[index]['food2']['name'],
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                              Text(
-                                list_name[index]['food2']['price'],
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 22.0,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                list_name[index]['food3']['name'],
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                              Text(
-                                list_name[index]['food3']['price'],
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                            ],
-                          ),
-                        ],
+                      padding: EdgeInsets.all(8.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              list_name[index]['title'],
+                              style: TextStyle(fontSize: 20.0),
+                              // key: dataKey,
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  list_name[index]['food1']['name'],
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                                Text(
+                                  list_name[index]['food1']['price'],
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  list_name[index]['food2']['name'],
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                                Text(
+                                  list_name[index]['food2']['price'],
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  list_name[index]['food3']['name'],
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                                Text(
+                                  list_name[index]['food3']['price'],
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
